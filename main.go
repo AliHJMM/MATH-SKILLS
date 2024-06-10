ef3924be3829b3367aa352d96ea032116a9d78cb
Ali <AliHasanJMM@gmail.com>
2024-06-11 02:47:24 +0300

    counted the average

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+func main() {
+	array, total, median, standardDeviation, average := []int{}, 0.0, 0, 0.0, 0.0
+	readingFile, err := os.Open("data.txt")
+	defer readingFile.Close()
+
+	if err != nil {
+		log.Fatalf("Error: Opening File Failure %s", err)
+	}
+
+	file := bufio.NewScanner(readingFile)
+	file.Split(bufio.ScanLines)
+
+	for file.Scan() {
+		parsedValue, err := strconv.Atoi(file.Text())
+		if err != nil {
+			fmt.Printf("%T \n %v", parsedValue, parsedValue)
+		}
+		array = append(array, parsedValue)
+		total += float64(parsedValue)
+	}
+	sort.Ints(array)
+
+	fmt.Print("Average: ")
+	average = math.Round(total / float64(len(array)))
+	fmt.Println(average)
+
+}
